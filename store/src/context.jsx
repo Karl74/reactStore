a56e4d98c2597bdcd839149ccd3e1c6d80e7cd03
karl74 <carlos.castro74@gmail.com>
2020-04-14 20:21:28 -0500

    cart module added

--- store/src/context.jsx
+++ store/src/context.jsx
@@ -8,8 +8,11 @@ class ProductProvider extends Component {
 		products:[],
 		detailProduct:detailProduct,
 		cart:[],
-		modalOpen:true,
+		modalOpen:false,
 		modalProduct: detailProduct,
+		cartSubTotal:0,
+		cartTax:0,
+		cartTotal:0
 	}
 	componentDidMount(){
 		this.setProducts();
@@ -46,7 +49,7 @@ class ProductProvider extends Component {
 			return {
 			products:tempProducts,
 			cart:[...this.state.cart, product]};
-		},()=>console.log(this.state.cart))
+		},()=>this.addTotals())
 	}
 
 	openModal = id =>{
@@ -59,7 +62,49 @@ class ProductProvider extends Component {
 	closeModal = () => {
 		this.setState ({modalOpen:false})
 	}
-		
+
+	increment =(id)=>{
+		console.log("this is the increment method")
+	}
+
+	decrement =(id)=>{
+		console.log("this is the decrement method")
+	}
+
+	removeItem =(id)=>{
+    let tempProducts = [...this.state.products]
+    let tempCart = [...this.state.cart]
+
+    tempCart = tempCart.filter(item => item.id !==id)
+    const index = tempProducts.indexOf(this.getItem(id))
+
+	}
+//5:25
+	clearCart = ()=>{
+		this.setState(()=>{
+      return {cart:[]}
+    },()=>{
+      this.setProducts()
+    }, ()=>{
+      this.addTotals()
+    })
+	}
+    
+  addTotals=()=>{
+    let subTotal = 0;
+    this.state.cart.map(item=>(subTotal+= item.total))
+    const tempTax = subTotal *0.1 
+    const tax = parseFloat(tempTax.toFixed(2))
+    const total = subTotal + tax
+    this.setState(()=>{
+      return {
+      cartSubTotal:subTotal,
+      cartTax: tax,
+      cartTotal: total
+    }
+  })
+  }
+  
 	render(){
 		return (
 			<ProductContext.Provider value={{
@@ -67,7 +112,12 @@ class ProductProvider extends Component {
 				handleDetail: this.handleDetail, 
 				addToCart: this.addToCart,
 				openModal:this.openModal,
-				closeModal:this.closeModal
+				closeModal:this.closeModal,
+				increment:this.increment,
+				decrement:this.decrement,
+				removeItem:this.removeItem,
+				clearCart:this.clearCart
+
 			}}>
 				{this.props.children}
 			</ProductContext.Provider>
